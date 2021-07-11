1940bce8916d1420f069cf28ea7fa1b0dccfa150
Aleksandr Hrytsenko <alkatraz0704@gmail.com>
2021-07-11 16:00:44 +0300

    to big size limits for webpack assets and entrypoint in build

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,18 @@
 const path = require("path");
-const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] },
+        use: {
+          loader: "babel-loader"
+        }
       },
       {
         test: /\.(png|svg|jpg|gif)$/i,
@@ -21,10 +20,6 @@ module.exports = {
         options: {
           name: "[path][name].[ext]",
         },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
       },
     ],
   },
@@ -34,21 +29,11 @@ module.exports = {
     publicPath: "/dist/",
     filename: "bundle.js",
   },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-  },
-  watch: true,
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "index.html",
     }),
-    new MiniCssExtractPlugin({
-      filename: "index.[hash:8].css",
-    }),
   ],
-  devtool: "source-map",
 };
