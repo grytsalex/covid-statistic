0102744a62291cbd158b21068cbad9407499d151
Aleksandr Hrytsenko <32422951+grytsalex@users.noreply.github.com>
2021-07-15 12:27:22 +0300

    delete netlify files, devServer works

--- webpack.prod.js
+++ webpack.prod.js
@@ -20,33 +20,4 @@ module.exports = merge(common, {
       filename: "index.[hash:8].css",
     }),
   ],
-  // optimization: {
-  //   minimizer: [
-  //     new OptimizeCssAssetsPlugin({
-  //       cssProcessorOptions: {
-  //         map: {
-  //           inline: false,
-  //           annotation: true,
-  //         },
-  //       },
-  //     }),
-  //     new TerserPlugin({
-  //       terserOptions: {
-  //         parse: {
-  //           ecma: 8,
-  //         },
-  //         compress: {
-  //           ecma: 5,
-  //           inline: 2,
-  //         },
-  //         output: {
-  //           ecma: 5,
-  //           comments: false,
-  //         }
-  //       },
-  //       parallel: true,
-  //       cache: true
-  //     }),
-  //   ],
-  // },
-});
+})
